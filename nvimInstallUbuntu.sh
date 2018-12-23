d6a37e19e11c2300ddea9cb609a17a3773aaa962
KamuiRoeru <me1802@s.akashi.ac.jp>
2018-12-23 10:13:09 +0900

    [modify]  apt-get -> apt

--- nvimInstallUbuntu.sh
+++ nvimInstallUbuntu.sh
@@ -1,7 +1,7 @@
 # python3が導入済みであるのが前提
-sudo apt-get install software-properties-common
+sudo apt install software-properties-common -y
 # sudo apt-get install python-software-properties
-sudo apt-add-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt-add-repository ppa:neovim-ppa/stable -y
+sudo apt update
+sudo apt install neovim -y
 pip install neovim
