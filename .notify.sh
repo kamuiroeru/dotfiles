c20472eb9e943ae9ffb93445253360479374c487
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2021-01-08 17:12:18 +0900

    notify.sh を改良

--- .notify.sh
+++ .notify.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# webhook_url が設定されてるかチェック
 webhook_url=$SLACK_BOT_INCOMING_WEBHOOK
 if [ -z $webhook_url ]; then
     exit 1
 fi
 
+# 送るメッセージを設定
 text="作業が終わりました。"
 if [ $# -ge 1 ]; then
     text="$(IFS=' '; echo $*)"
 fi
 
-echo $#
-echo $text
+# _ADD_TIMEが指定されていれば、時刻を前に追加する
+if [ -v _ADD_TIME ]; then
+    text=$(date "+%Y/%m/%d %H:%M:%S.%3N")"  ${text}"
+fi
 
 curl \
     -s \
